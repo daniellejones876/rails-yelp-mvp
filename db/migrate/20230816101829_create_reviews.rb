f5cbc32b62a7af457799f267b17a7da37bf0e53f
Danielle Jones <daniellejones_97@yahoo.com>
2023-08-16 11:42:09 +0100

    created a successful migration

---
+++ db/migrate/20230816101829_create_reviews.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateReviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reviews do |t|
+      t.text :content
+      t.references :restaurant, null: false, foreign_key: true
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 10.times do
+#   User.create.(
+#     name: Faker::Name.name,
+#     phone_number: Faker::Internet.email,
+#     address: Faker::Address.full_address,
+#     category: Faker::
+#   )
+# end
+# puts
